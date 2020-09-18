42c825a8494a956781d25264ce9e7e6e7ae384fc
Paweł Idzikowski <iddziku@gmail.com>
2020-09-18 13:57:40 +0200

    Modify get_biggest_even_sum with mode(even,uneven), some fixes

--- mcs/main.py
+++ mcs/main.py
@@ -1,42 +1,57 @@
 # This is sample in Python for
 # Most Common (Even/Uneven) Sum of given list
+# created by https://github.com/trolit
 
 # Press Shift+F10 to execute it.
 
-def get_biggest_even_sum(list_numbers, var_size):
+# mode => even/uneven
+def get_biggest_sum_depending_on_mode(list_numbers, var_size, var_mode):
     # Use a breakpoint in the code line below to debug your script.
-    list_numbers.sort(reverse=True)
     list_numbers_length = len(list_numbers)
+    if var_size > list_numbers_length or var_size < 2 or var_mode != 'even' and var_mode != 'uneven':
+        return
+    list_numbers.sort(reverse=True)
     list_chosen_numbers = []
-    var_biggest_even_sum = 0
-    var_is_even_sum_found = False
+    var_biggest_sum = 0
+    var_is_sum_found = False
     var_starting_pos = 0
     while 1:
         list_chosen_numbers = []
         var_index = var_starting_pos
         var_current_sum = 0
-        while var_index < var_size - 1:
+        var_number_counter = 0
+        while var_number_counter < var_size - 1:
             var_current_sum += list_numbers[var_index]
             list_chosen_numbers.append(list_numbers[var_index])
             var_index += 1
+            var_number_counter += 1
+        if var_number_counter + 1 != var_size:
+            print('For: [', return_elements_of_list(list_numbers), ']')
+            print(f'Couldn\'t find {var_size} numbers ({var_mode} mode)')
+            print()
+            break
         while var_index < list_numbers_length:
             var_number = list_numbers[var_index]
             var_test_sum = var_current_sum + var_number
-            if var_test_sum % 2 == 0:
-                var_biggest_even_sum = var_test_sum
-                var_is_even_sum_found = True
+            if (var_test_sum % 2 == 0 and var_mode == 'even') or (var_test_sum % 2 != 0 and var_mode == 'uneven'):
+                list_chosen_numbers.append(var_number)
+                var_biggest_sum = var_test_sum
+                var_is_sum_found = True
+                print('For: [', return_elements_of_list(list_numbers), '],', var_mode, '(', var_size, ')')
+                print('selected:', return_elements_of_list(list_chosen_numbers))
+                print('which gives:', var_biggest_sum)
+                print()
                 break
             var_index += 1
-        if not var_is_even_sum_found:
+        if not var_is_sum_found:
             var_starting_pos += 1
         else:
             break
-    print('For: [', elements_of_list(list_numbers), ']')
-    print('selected:', elements_of_list(list_chosen_numbers))
-    print('which gives: ', var_biggest_even_sum)
+        if var_starting_pos >= list_numbers_length:
+            break
 
 
-def elements_of_list(user_list):
+def return_elements_of_list(user_list):
     var_list_elements = ''
     var_i = 0
     for list_item in user_list:
@@ -48,6 +63,10 @@ def elements_of_list(user_list):
     return var_list_elements
 
 
-get_biggest_even_sum([1, 2, 9, 3, 6, 6, 7, 7, 3], 4)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Examples
+# get_biggest_sum_depending_on_mode([1, 2, 9, 3, 6, 6, 7, 7, 3], 4, 'even')
+# get_biggest_sum_depending_on_mode([102, 15, 33, 4, 81, 2, 13, 6, 79], 4, 'even')
+# get_biggest_sum_depending_on_mode([4, 32, 15, 1, 6, 2, 8, 6, 9], 3, 'uneven')
+# get_biggest_sum_depending_on_mode([0, 0, 3, 2, 0, 5, 2, 7, 6], 7, 'uneven')
+# get_biggest_sum_depending_on_mode([3, 32, 5], 3, 'uneven')
+get_biggest_sum_depending_on_mode([1, 1, 1, 2, 3], 3, 'uneven')
